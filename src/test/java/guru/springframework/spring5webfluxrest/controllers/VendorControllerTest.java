cf6de745ef28406ffb79dd930a4da2416b0bbc5c
Shenoy <54286073+shenoy@users.noreply.github.com>
2021-09-18 21:16:43 +0100

    added vendor controller and test

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @Before
+    public void setUp() throws Exception {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("Joe").lastName("Buck").build(),
+                        Vendor.builder().firstName("Mike").lastName("West").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors/")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(vendorRepository.findById("someId"))
+                .willReturn(Mono.just(Vendor.builder().firstName("Cathy").lastName("Jenkins").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors/someId")
+                .exchange()
+                .expectBody(Category.class);
+
+    }
+}
